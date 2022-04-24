0a424445d3dae1e53282c7aaf0378eeb0c65f408
MengHao Tian <907749695@qq.com>
2022-04-24 21:47:50 +0800

    feat: 项目初始化

---
+++ src/main/java/org/blue/automation/Main.java
@@ -0,0 +1,40 @@
+package org.blue.automation;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.opencv.core.Core;
+
+public class Main extends Application {
+    private static final Logger log = LogManager.getLogger(Main.class);
+    public static Stage PRIMARY_STAGE;
+
+    @Override
+    public void init() {
+        try {
+            System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+            log.info("opencv链接库加载完成");
+        }catch (Exception e){
+            log.error("opencv链接库加载失败");
+        }
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        PRIMARY_STAGE = primaryStage;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/index.fxml"));
+        Parent root = fxmlLoader.load();
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+        log.info("程序启动完成");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/org/blue/automation/controller/IndexController.java
@@ -0,0 +1,19 @@
+package org.blue.automation.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * name: MengHao Tian
+ * date: 2022/4/24 15:08
+ */
+public class IndexController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+}
